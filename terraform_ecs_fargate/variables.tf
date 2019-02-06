3f31fa163f72fef7559c03e32797872355a51a97
alanzha0598 <alanazha0598@hotmail.com>
2019-02-06 15:29:07 -0500

    Initial commit

--- terraform_ecs_fargate/variables.tf
+++ terraform_ecs_fargate/variables.tf
@@ -14,7 +14,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "448811623389.dkr.ecr.us-east-1.amazonaws.com/qa-nginx:latest"
+  default     = "448811623389.dkr.ecr.us-east-1.amazonaws.com/ecs-nginx:latest"
 }
 
 variable "app_port" {
